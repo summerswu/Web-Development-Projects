0e8973c894faf4cdbb310015b99be30dd65811d6
summers wu <wlysummers@gmail.com>
2019-05-19 16:50:48 -0400

    adding working coment prototype for the application

--- Yelpcamp/app.js
+++ Yelpcamp/app.js
@@ -1,32 +1,33 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
 
+var seedDB = require("./seeds.js");
+
+seedDB();
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true });
 app.set("view engine", "ejs")
-app.use(bodyParser.urlencoded({extended:true})); 
-
-var campgrounds = [
-        {name: "Salme Creek", image: "http://www.camp-liza.com/wp-content/uploads/2017/10/20170708_093155_HDR-1.jpg" },
-        {name: "Graphite Hill", image: "https://www.pinetreesociety.org/wp-content/uploads/2017/10/cabins-960x600.jpg"},
-        {name: "Goatee", image: "https://images.unsplash.com/photo-1534880606858-29b0e8a24e8d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
-        {name: "Salme Creek", image: "http://www.camp-liza.com/wp-content/uploads/2017/10/20170708_093155_HDR-1.jpg" },
-        {name: "Graphite Hill", image: "https://www.pinetreesociety.org/wp-content/uploads/2017/10/cabins-960x600.jpg"},
-        {name: "Goatee", image: "https://images.unsplash.com/photo-1534880606858-29b0e8a24e8d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
-        {name: "Salme Creek", image: "http://www.camp-liza.com/wp-content/uploads/2017/10/20170708_093155_HDR-1.jpg" },
-        {name: "Graphite Hill", image: "https://www.pinetreesociety.org/wp-content/uploads/2017/10/cabins-960x600.jpg"},
-        {name: "Graphite Hill", image: "https://www.pinetreesociety.org/wp-content/uploads/2017/10/cabins-960x600.jpg"},
-        {name: "Goatee", image: "https://images.unsplash.com/photo-1534880606858-29b0e8a24e8d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80"},
-        {name: "Salme Creek", image: "http://www.camp-liza.com/wp-content/uploads/2017/10/20170708_093155_HDR-1.jpg" },
-        {name: "Graphite Hill", image: "https://www.pinetreesociety.org/wp-content/uploads/2017/10/cabins-960x600.jpg"}
-    ];
-    
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static( __dirname + "/public"));
+console.log( __dirname );
 
 app.get("/", function(req, res){
    res.render("landing"); 
 });
 
 app.get("/campgrounds", function(req,res){
-    res.render("campgrounds",{campgrounds:campgrounds});
+    Campground.find({},function(err,allcampground){
+        if(err){
+            console.log(err);
+        }
+        else{
+             res.render("index",{campgrounds:allcampground});
+        }
+    });
 });
 
 app.post("/campgrounds", function(req,res){
@@ -34,13 +35,66 @@ app.post("/campgrounds", function(req,res){
    var name = req.body.name;
    var image = req.body.image;
    var newCampground = {name: name, image: image}
-   campgrounds.push(newCampground); 
-   res.redirect("/campgrounds");
+   Campground.create(newCampground, function(err, newlyCreated){
+       if(err){
+           console.log(err);
+       }
+       else{
+           res.redirect("/campgrounds");
+       }
+   });
 });
 
 app.get("/campgrounds/new", function(req,res){
    //get data from form and add to the campgrounds array
-   res.render("new");
+   res.render("./campgrounds/new");
+});
+
+app.get("/campgrounds/:id", function(req,res){
+   //get data from form and add to the campgrounds array
+   Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+      if(err){
+          console.log(err);
+      } 
+      else{
+           res.render("./campgrounds/show", {campgrounds: foundCampground});
+      }
+   });
+  
+});
+
+app.post("/campgrounds/:id/comments", function(req,res){
+   //get data from form and add to the campgrounds array
+   Campground.findById(req.params.id, function(err, foundCampground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else{
+           Comment.create(req.body.comment, function(err, comment){
+               if(err){
+                   console.log(err);
+                   res.redirect("/campgrounds");
+               }
+               else{
+                   foundCampground.comments.push(comment);
+                   foundCampground.save();
+                   res.redirect("/campgrounds/" + foundCampground._id)
+               }
+           });
+       }
+   });
+});
+
+app.get("/campgrounds/:id/comments/new", function(req,res){
+   //get data from form and add to the campgrounds array\
+   Campground.findById(req.params.id,function(err, foundCampground){
+       if(err){
+           console.log(err);
+       }
+       else{
+           res.render("./comments/new", {campgrounds: foundCampground});
+       }
+   })
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
