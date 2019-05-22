752f7d3a5e1dc9adbe1172799a0a8032c9f1be85
summers wu <wlysummers@gmail.com>
2019-05-22 16:42:03 -0400

    working authen mock up with added middleware to routes

--- authen/authDemo/app.js
+++ authen/authDemo/app.js
@@ -12,16 +12,17 @@ var app = express();
 
 app.set("view engine", "ejs"); 
 
-app.use(passport.initialize());
-app.use(bodyParser.urlencoded({extend: true}));
-app.use(passport.session());
-
-
 app.use(require("express-session")({
     secret: "Rusty the cutest",
     resave: false,
     saveUnitialized: false
 }));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({extend: true}));
+
 
 passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser()); 
@@ -31,7 +32,7 @@ app.get("/", function(req,res){
     res.render("home");
 });
 
-app.get("/secret", function(req,res){
+app.get("/secret", isLoggedIn, function(req,res){
     res.render("secret");
 });
 
@@ -65,6 +66,21 @@ app.post("/login", passport.authenticate("local",{
 }), function(req,res){
     
 });
+
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
+function isLoggedIn( req, res, next){
+    console.log(req.isAuthenticated());
+    if(req.isAuthenticated()){
+        return next();
+    } else{
+        res.redirect("/login");
+        console.log("failed");
+    }
+}
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("server have started"); 
